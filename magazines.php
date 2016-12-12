fae8a7e1323b2f0d311b005a99dd4db40d85dcf0
diogosferreira <diogoferreirasgr@gmail.com>
2016-12-12 12:26:20 +0000

    tirar

--- magazines.php
+++ magazines.php
@@ -9,88 +9,88 @@ else
 ?>
 
 
-<html>
+    <html>
 
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title> Spine </title>
-    <link rel="icon" href="images/minilogo.png">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title> Spine </title>
+        <link rel="icon" href="images/minilogo.png">
 
-    <meta name="Spine" content="An interactive plataform to sell the coolest magazines.">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="Spine" content="An interactive plataform to sell the coolest magazines.">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="css/fonts.css">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/magazines.css">
-</head>
+        <link rel="stylesheet" href="css/fonts.css">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/magazines.css">
+    </head>
 
-<body class="index">
-    <header>
-        <a href="index.php"> <img src="images/logo.png" alt="spinelogo" id="logo"> </a>
-        <div id="login"> <a href="login.php">Login</a> / <a href="register.php">Register</a></div>
-    </header>
+    <body class="index">
+        <header>
+            <a href="index.php"> <img src="images/logo.png" alt="spinelogo" id="logo"> </a>
+            <div id="login"> <a href="login.php">Login</a> / <a href="register.php">Register</a></div>
+        </header>
 
 
-    <nav>
-        <ul>
-            <li> <a href="magazines.html"> Magazines </a></li>
-            <li> <a href="about.html"> About </a></li>
-            <li> <a href="contacts.html"> Contact </a></li>
-        </ul>
+        <nav>
+            <ul>
+                <li> <a href="magazines.html"> Magazines </a></li>
+                <li> <a href="about.html"> About </a></li>
+                <li> <a href="contacts.html"> Contact </a></li>
+            </ul>
 
-        <br>
+            <br>
 
-        <div id="dropdown-list">
-            <div class="dropdown closed">
-                <div class="title">Pick Category</div>
+            <div id="dropdown-list">
+                <div class="dropdown closed">
+                    <div class="title">Pick Category</div>
 
-                <div class="dropdown-menu">
-                    <ul id="list">
-                        <li id="all">All</li>
-                        <li id="design">Design</li>
-                        <li id="social">Social</li>
-                        <li id="illustration">Illustration</li>
-                    </ul>
+                    <div class="dropdown-menu">
+                        <ul id="list">
+                            <li id="all">All</li>
+                            <li id="design">Design</li>
+                            <li id="social">Social</li>
+                            <li id="illustration">Illustration</li>
+                        </ul>
+                    </div>
+
                 </div>
+            </div>
+        </nav>
+
+
+
+        <section id="postPage">
+            <div id="posts">
+
+                <!--  cria as divs aqui   -->
 
             </div>
-        </div>
-    </nav>
 
 
 
-    <section id="postPage">
-        <div id="posts">
 
-            <!--  cria as divs aqui   -->
+            <div id="left" class="column">
 
-        </div>
-        
-        
-        
-        
-        <div id="left" class="column">
-            
-        </div>
-        
-    </section>
+            </div>
+
+        </section>
 
 
-    <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
-    <script src="js/script.js"></script>
+        <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+        <script src="js/script.js"></script>
 
-    <script type="text/javascript" language="javascript">
-        var user = <?php echo json_encode($user); ?>;
-        if (user)
-            $('#login').html('<a href="welcome.php">Profile</a> / <a href="logout.php">Logout</a>');
-        console.log(user);
-    
-        
-        for (i = 0; i < 8; i++) {
-            var palmas = <?php echo json_encode($didi); ?>;
-            //console.log("i é: " +palmas[i].categoria);
-        }
+        <script type="text/javascript" language="javascript">
+            var user = <?php echo json_encode($user); ?>;
+            if (user)
+                $('#login').html('<a href="welcome.php">Profile</a> / <a href="logout.php">Logout</a>');
+            console.log(user);
+
+
+            for (i = 0; i < 8; i++) {
+                var palmas = <?php echo json_encode($didi); ?>;
+                //console.log("i é: " +palmas[i].categoria);
+            }
             showAll();
             //ver qual a categoria selecionada
 
@@ -133,7 +133,7 @@ else
 
             });
 
-            
+
 
 
             function showAll() {
@@ -148,19 +148,18 @@ else
                     $('#div' + i).prepend("<a href='revistasCodbarras/" + codBarras + ".html'> <img src='imagesCodbarras/" + codBarras + ".jpg'> </a>");
                 }
             }
-            
-        
-            
-            
-            
-            
+
+
+
+
+
+
             //teste de mostrar revista
-            console.log("corre");
-            var codBarras = palmas[1].codBarras;
-            $('#left').prepend("<img class='cover' src='imagesCodbarras/" + codBarras + ".jpg'> </a>");
-            
+            // console.log("corre");
+            //var codBarras = palmas[1].codBarras;
+            //$('#left').prepend("<img class='cover' src='imagesCodbarras/" + codBarras + ".jpg'> </a>");
         </script>
 
-</body>
+    </body>
 
-</html>
+    </html>
