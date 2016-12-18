261f9fc39d0dcfa1ce04b3bdef272b0778150494
diogosferreira <diogoferreirasgr@gmail.com>
2016-12-18 00:27:27 +0000

    pesquisa meia

--- magazines.php
+++ magazines.php
@@ -9,6 +9,10 @@ else
 ?>
 
 
+
+
+
+
     <html>
 
     <head>
@@ -28,7 +32,11 @@ else
     <body class="index">
         <header>
             <a href="index.php"> <img src="images/logo.png" alt="spinelogo" id="logo"> </a>
+
+
             <div id="login"> <a href="login.php">Login</a> / <a href="register.php">Register</a></div>
+
+
         </header>
 
 
@@ -62,96 +70,168 @@ else
             <!--<form action="form.php" method="post">-->
             <form method="post">
                 Search:
-                <input type="text" name="option" />
-                <br />
+                <br>
+                <br>
+                <input class="inputtext" type="text" name="option" placeholder="Search Products" />
+                <br/>
+                <br>
                 <input type="submit" value="Search" />
             </form>
             <!--  search   -->
 
         </nav>
 
-        
-        
-    
 
-        <section id="postPage">
-            <div id="posts">
-                <!--  cria as divs aqui   -->
+        <!--————————————————   Search   —————————————————-->
+
+        <?php 
+        
+        $option = $_POST["option"];  //o que foi escrito na pesquisa
+
+        $sql = "SELECT nomeRevista, idRevista FROM revistaNum WHERE nomeRevista LIKE '%$option%'";
+
+        $result = $conn->query($sql);
+
+        $contador=0;
+
+        if ($result->num_rows > 0  ) {
+               
+            //
+
+            while($row = $result->fetch_assoc()) {
+                
+                $pesquisa[$contador] = array("nome"=>$row["nomeRevista"], "id"=>$row["idRevista"]);
+                $contador++; 
+                echo "<div class=" . "posts" . ">" . $row["nomeRevista"] . "</div>";  
+            
+            }
+        } else {
+            echo "No products were found matching your selection.";
+        }
+        
+        ?>
+
+            <!--—————————————   Search   ————————————————————-->
+
+
+            <section id="postPage">
+                <div id="posts">
+                    <!--  cria as divs aqui   -->
+                </div>
+
+            </section>
+
+
+
+
+            <div id="left" class="column">
+
             </div>
 
-        </section>
+
+            <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+
+
+            <script type="text/javascript" language="javascript">
+                /* ———————  search magazines  ————————— */
+
+
+
+                for (i = 0; i < 8; i++) {
+                    var resultadoPesquisa = <?php echo json_encode($pesquisa); ?>;
+                    //var procura = resultadoPesquisa[i].id;
+                }
+                
+                
+                
+                for (i = 0; i < 8; i++) {
+                 console.log("top  " + resultadoPesquisa[i].id);
+                
+                
+                var procura = resultadoPesquisa[i].id;
+
+                            
+                                jQuery('<div/>', {
+                                    id: '' + procura,
+                                    class: 'post',
+                                    // text: 'div bem criada' + i
+                                }).appendTo('#posts'); //id/class so sitio
+
+                                // meter id e imagem em variável
+
+                                $('#' + procura).prepend("<a href='baseRevista.php'> <img src='images/mags/" + procura + ".jpg'> </a>");
+                            
+                
+                
+                
+                }
+                
+                
+                /* ———————  search magazines  ————————— */
 
 
 
 
-        <div id="left" class="column">
 
-        </div>
-
-
-        <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
-
-
-        <script type="text/javascript" language="javascript">
-            var testeaula = $('#posts').text();
-            var user = <?php echo json_encode($user); ?>;
-            if (user)
-                $('#login').html('<a href="welcome.php">Profile</a> / <a href="logout.php">Logout</a>');
-            console.log(user);
+                var testeaula = $('#posts').text();
+                var user = <?php echo json_encode($user); ?>;
+                if (user)
+                    $('#login').html('<a href="welcome.php">Profile</a> / <a href="logout.php">Logout</a>');
+                console.log(user);
 
 
-            for (i = 0; i < 8; i++) {
-                var palmas = <?php echo json_encode($didi); ?>;
-                //console.log("i é: " +palmas[i].categoria);
-            }
-            showAll();
-            //ver qual a categoria selecionada
+                for (i = 0; i < 8; i++) {
+                    var palmas = <?php echo json_encode($didi); ?>;
+                    //console.log("i é: " +palmas[i].categoria);
+                }
+                showAll();
+                //ver qual a categoria selecionada
 
 
-            $("#list li").on("click", function () {
-                var divNome = $(this).attr('id');
+                $("#list li").on("click", function () {
+                    var divNome = $(this).attr('id');
 
-$( "div" ).remove( ".post" );
-                if (divNome === 'all') {
-                    showAll();
-                } else {
-                    for (i = 0; i < 8; i++) {
-                        var id = palmas[i].id;
+                    $("div").remove(".post");
+                    if (divNome === 'all') {
+                        showAll();
+                    } else {
+                        for (i = 0; i < 8; i++) {
+                            var id = palmas[i].id;
 
-                        if (divNome === palmas[i].categoria) {
-                            jQuery('<div/>', {
-                                id: '' + id,
-                                class: 'post',
-                                // text: 'div bem criada' + i
-                            }).appendTo('#posts'); //id/class so sitio
+                            if (divNome === palmas[i].categoria) {
+                                jQuery('<div/>', {
+                                    id: '' + id,
+                                    class: 'post',
+                                    // text: 'div bem criada' + i
+                                }).appendTo('#posts'); //id/class so sitio
 
-                            // meter id e imagem em variável
+                                // meter id e imagem em variável
 
-                            $('#' + id).prepend("<a href='baseRevista.php'> <img src='images/mags/" + id + ".jpg'> </a>");
+                                $('#' + id).prepend("<a href='baseRevista.php'> <img src='images/mags/" + id + ".jpg'> </a>");
 
+                            }
                         }
                     }
+                });
+
+
+
+                //mostrar todas as revistas
+                function showAll() {
+                    for (i = 0; i < 8; i++) {
+                        var id = palmas[i].id;
+                        jQuery('<div/>', {
+                            id: '' + id,
+                            class: 'post',
+                            // text: 'div bem criada' + i
+                        }).appendTo('#posts'); //id/class so sitio
+
+                        //$('#div' + i).prepend("<a href='revistasCodbarras/" + codBarras + ".html'> <img src='imagesCodbarras/" + codBarras + ".jpg'> </a>");
+
+                        $('#' + id).prepend("<a href='baseRevista.php'> <img src='images/mags/" + id + ".jpg'> </a>");
+                    }
                 }
-            });
-
-
-
-            //mostrar todas as revistas
-            function showAll() {
-                for (i = 0; i < 8; i++) {
-                    var id = palmas[i].id;
-                    jQuery('<div/>', {
-                        id: '' + id,
-                        class: 'post',
-                        // text: 'div bem criada' + i
-                    }).appendTo('#posts'); //id/class so sitio
-
-                    //$('#div' + i).prepend("<a href='revistasCodbarras/" + codBarras + ".html'> <img src='imagesCodbarras/" + codBarras + ".jpg'> </a>");
-
-                    $('#' + id).prepend("<a href='baseRevista.php'> <img src='images/mags/" + id + ".jpg'> </a>");
-                }
-            }
-        </script>
+            </script>
 
 
     </body>
--- search.php
+++ search.php
@@ -8,7 +8,7 @@ echo "cookie " . $id;
 
 $option = $_POST["option"];  //o que foi escrito na pesquisa
 
-$sql = "SELECT nomeRevista FROM revistaNum WHERE nomeRevista LIKE '%$option%'";
+$sql = "SELECT DISTINCT nomeRevista FROM revistaNum WHERE nomeRevista LIKE '%$option%'";
 
 $result = $conn->query($sql);
 
@@ -21,8 +21,7 @@ if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
         //$didi[$contador] = array("nome"=>$row["nomeRevista"]);
         //$contador++;
-        echo "<div> $row["nomeRevista"] </div>";
-        
+        echo "<div> $row["nomeRevista"] </div>";  
     }
 } else {
     echo " 0 results";
